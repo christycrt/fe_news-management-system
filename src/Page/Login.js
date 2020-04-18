40175c35a558d262aa5c65504a54e17ec73e81ef
christycrt <christycrt@gmail.com>
2020-04-18 22:35:49 +0700

    [UPDATE] Background and Picture

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { ButtonLineLogin } from '../Component/LoginPage/ButtonLogin'
-import { ButtonFacebookLogin } from '../Component/LoginPage/ButtonLogin'
-import { ButtonGoogleLogin } from '../Component/LoginPage/ButtonLogin'
+import { ButtonLogin } from '../Component/LoginPage/ButtonLogin'
 import styled from 'styled-components';
 
 const Page = styled.div`
     height: 100vh;
     position: relative;
+    background-image: url("image/Login/bg-mobile.png");
+    background-size: 100% 100%;
 `
 
 const Content = styled.div`
@@ -24,18 +24,19 @@ class LoginPage extends Component {
                 <Content className="col-10 p-0">
                     <div className="col-12 row m-0 p-0">
                         <div className="col-12 col-sm-6 d-none d-sm-block">
-                            <img src="image/logo.png" alt="logo" width="100%" />
+                            {/* <img src="image/Login/bg-mobile.png" width="100%"/> */}
+                            <img src="image/pic news.png" alt="pic news" width="100%" />
                         </div>
                         <div className="col-12 col-sm-6">
                             <div className="col-12 p-0">
-                                <div className="col-6 p-0 mx-auto d-sm-none">
-                                    <img src="image/logo.png" alt="logo" width="100%" />
+                                <div className="col-10 p-0 mx-auto d-sm-none">
+                                    <img src="image/pic news.png" alt="pic news" width="100%" />
                                 </div>
                                 <h1 className="text-center">SIGN IN</h1>
                                 <div className="col-10 p-0 mx-auto">
-                                    <div className="mb-3"><ButtonLineLogin /></div>
-                                    <div className="mb-3"><ButtonFacebookLogin /></div>
-                                    <div><ButtonGoogleLogin /></div>
+                                    <div className="mb-3"><ButtonLogin>Line</ButtonLogin></div>
+                                    <div className="mb-3"><ButtonLogin>Facebook</ButtonLogin></div>
+                                    <div><ButtonLogin>Google</ButtonLogin></div>
                                 </div>
                             </div>
                         </div>
