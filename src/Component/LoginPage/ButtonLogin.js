b564f7c841300a34fa74348380ddb79acc394d2d
christycrt <christycrt@gmail.com>
2020-04-18 22:35:10 +0700

    [UPDATE] Button Login

--- src/Component/LoginPage/ButtonLogin.js
+++ src/Component/LoginPage/ButtonLogin.js
@@ -6,23 +6,14 @@ const Button = styled.button`
   padding:8px;
   border-radius:30px;
   border:none;
-  background-image: linear-gradient(${props => props.firstcolor}, ${props => props.secondcolor});
+  background-color:#0095FF;
   color:white;
   font-size:20px;
 `;
 
-export const ButtonLineLogin = (props) => {
-    return <Button firstcolor="#71DA95" secondcolor="#A9DB8E"><img className="mr-2" src="image/Login/line.png" alt="line" width="25px"/>Sign in with Line</Button>
+export const ButtonLogin = (props) => {
+return <Button><img className="mr-2" src={`image/Login/${props.children}.png`} alt="line" width="25px"/>Sign in with {props.children}</Button>
 }
-
-export const ButtonFacebookLogin = (props) => {
-    return <Button firstcolor="#2C60AB" secondcolor="#41BACF"><img className="mr-2" src="image/Login/facebook.png" alt="facebook" width="25px"/>Sign in with Facebook</Button>
-}
-
-export const ButtonGoogleLogin = (props) => {
-    return <Button firstcolor="#E46F77" secondcolor="#F8978F"><img className="mr-2" src="image/Login/google.png" alt="google" width="25px"/>Sign in with Google</Button>
-}
-
 
 // export const Button = (props) => {
 //     return <div className='btn btn-warning'>{props.children}</div>
